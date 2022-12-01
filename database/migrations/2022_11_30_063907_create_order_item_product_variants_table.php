74f7ce1abdc63bc3923df780ee3cf93b6813f8b0
Rahmy Athas <rahmyathasamrd@gmail.com>
2022-12-01 18:06:34 +0530

    migration

---
+++ database/migrations/2022_11_30_063907_create_order_item_product_variants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_item_product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_item_id');
+            $table->foreignId('product_variant_id');
+            $table->timestamps();
+
+            $table->unique(['order_item_id','product_variant_id'],'unique_order_item_product_variant');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_item_product_variants');
+    }
+};
