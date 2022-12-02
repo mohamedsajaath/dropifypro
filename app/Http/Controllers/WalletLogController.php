388642dfdb0faad3572c450ee50691e26b2208ce
Rahmy Athas <rahmyathasamrd@gmail.com>
2022-12-02 13:49:45 +0530

    modals and controllers added

---
+++ app/Http/Controllers/WalletLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WalletLog;
+use Illuminate\Http\Request;
+
+class WalletLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WalletLog  $walletLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WalletLog $walletLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WalletLog  $walletLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WalletLog $walletLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WalletLog  $walletLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WalletLog $walletLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WalletLog  $walletLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WalletLog $walletLog)
+    {
+        //
+    }
+}
