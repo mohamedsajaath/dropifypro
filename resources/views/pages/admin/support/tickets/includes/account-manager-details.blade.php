3f247a3d1c018e7142d900ea9b1b3ea47e1014da
nafrisnafris <lareefnafris7086@gmail.com>
2022-12-15 14:21:04 +0530

    admin tickets account manager page

---
+++ resources/views/pages/admin/support/tickets/includes/account-manager-details.blade.php
@@ -0,0 +1,56 @@
+<!--begin::details View-->
+<div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+    <!--begin::Card body-->
+    <div class="card-body p-9">
+        <!--begin::Row-->
+        <div class="row mb-7">
+            <!--begin::Label-->
+            <label class="col-lg-4 fw-semibold text-muted">MANAGER NAME</label>
+            <!--end::Label-->
+            <!--begin::Col-->
+            <div class="col-lg-8">
+                <span class="fw-bold fs-6 text-gray-800">Patric Watson</span>
+            </div>
+            <!--end::Col-->
+        </div>
+        <!--end::Row-->
+        <!--begin::Input group-->
+        <div class="row mb-7">
+            <!--begin::Label-->
+            <label class="col-lg-4 fw-semibold text-muted">E-MAIL</label>
+            <!--end::Label-->
+            <!--begin::Col-->
+            <div class="col-lg-8 fv-row">
+                <span class="fw-bold fs-6 text-gray-800">watson123@gmail.com</span>
+            </div>
+            <!--end::Col-->
+        </div>
+        <!--end::Input group-->
+        <!--begin::Input group-->
+        <div class="row mb-7">
+            <!--begin::Label-->
+            <label class="col-lg-4 fw-semibold text-muted">WHATSAPP NO</label>
+            <!--end::Label-->
+            <!--begin::Col-->
+            <div class="col-lg-8 d-flex align-items-center">
+                <span class="fw-bold fs-6 text-gray-800">044 3276 454 935</span>
+            </div>
+            <!--end::Col-->
+        </div>
+        <!--end::Input group-->
+        <!--begin::Input group-->
+        <div class="row mb-7">
+            <!--begin::Label-->
+            <label class="col-lg-4 fw-semibold text-muted">RESPONSE TIME</label>
+            <!--end::Label-->
+            <!--begin::Col-->
+            <div class="col-lg-8">
+                <a href="#" class="fw-semibold fs-6 text-gray-800 text-hover-primary">1-2 hours</a>
+            </div>
+            <!--end::Col-->
+        </div>
+        <!--end::Input group-->
+    </div>
+    <!--end::Card body-->
+</div>
+<!--end::details View-->
