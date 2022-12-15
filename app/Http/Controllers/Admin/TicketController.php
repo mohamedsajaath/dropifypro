26492ec91ac8b5d63a161e2cb7a90da3265f4a76
nafrisnafris <lareefnafris7086@gmail.com>
2022-12-15 14:25:14 +0530

    ticket controller

---
+++ app/Http/Controllers/Admin/TicketController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Ticket;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('pages.admin.support.tickets.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.admin.support.tickets.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //get all request data as array
+        $inputs = $request->all();
+
+        //create new record
+        $newTickets = new Ticket();
+        $newTickets->fill($inputs);
+
+        // dd($newAccountManager);
+        $newTickets->save();
+
+        $result = "result";
+        $message = "Succesfully insert";
+        $errors = [];
+        $status = 200;
+
+        return response()->json(['result' => $result, 'message' => $message, 'errors' => $errors], $status);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
