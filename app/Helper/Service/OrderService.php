d96df33fbadcf37de0741d960e4cd2b580b88a09
Rahmy Athas <rahmyathasamrd@gmail.com>
2022-12-29 10:56:49 +0530

    order pages changes and layouts pages changes

--- app/Helper/Service/OrderService.php
+++ app/Helper/Service/OrderService.php
@@ -9,7 +9,7 @@ class OrderService
     public static function getOrderById($id)
     {
         $orders = Order::join('users', 'orders.user_id', '=', 'users.id')
-            ->select('orders.*', 'users.first_name as fname')
+            ->select('orders.*', 'users.name as uname')
             ->where('orders.user_id', '=', $id)
             ->get();
         return $orders;
@@ -22,7 +22,7 @@ class OrderService
             ->join('order_item_product_variants', 'order_items.id', '=', 'order_item_product_variants.order_item_id')
             ->join('product_variants', 'product_variants.id', '=', 'order_item_product_variants.product_variant_id')
             ->join('products', 'product_variants.product_id', '=', 'products.id')
-            ->select('orders.*', 'order_items.*','order_items.quantity as item_quantity', 'order_customers.name as cname', 'products.*', 'product_variants.*')
+            ->select('orders.*', 'order_items.*', 'order_customers.name as cname', 'products.*', 'product_variants.*')
             ->where('order_items.order_id', '=', $id)
             ->get();
         return $order_items;
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/image-uploader/image-uploader.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
     <link href="{{ asset('common.css') }}" rel="stylesheet" type="text/css" />
     @stack('css')
